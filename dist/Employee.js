26cc46ec48b112188216def05142948e68ee3dcc
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-18 18:36:50 -0500

    WIP

--- dist/Employee.js
+++ dist/Employee.js
@@ -5,7 +5,27 @@ class Employee {
         this.id = id;
         this.email = email 
     }
+    // return name from input
+    getName () {
+        return this.name;
+    }
+
+     // return ID from input
+     getId () {
+        return this.id;
+    } 
+
+    // returning email from input
+    getEmail () {
+        return this.email;
+    }
+
+    // returning employee type 
+    getRole () {
+        return 'Employee'; 
+    }
 };    
 
+// to be exported
     module.exports = Employee;
   
