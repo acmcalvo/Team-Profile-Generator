150a9454ead83f7ac90c9ead0dd1a55d874604f7
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-21 18:56:05 -0500

    start added input

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+// My team profile
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern'); 
+
  const fs = require('fs');
  const inquirer= require('inquirer');
+
+ const teamArray = [];
+
+ const promptUser = () => {
+     return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Who is the manager of this team?', 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the manager's name!");
+                    return false; 
+                }
+            }
+        },
+
+     ])
+ }
