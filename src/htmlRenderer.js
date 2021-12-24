414c2a5a8e21659fe9d6298f98e72e14bdf30101
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-24 08:15:38 -0500

    WIP

--- src/htmlRenderer.js
+++ src/htmlRenderer.js
@@ -106,18 +106,20 @@ const generateTeamPage = function (employeeCards) {
   <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Team Profile</title>
+      <title>Team Profile| ACM</title>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
       <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
   </head>
+  </head>
   <body>
-      <header>
-          <nav class="navbar" id="navbar">
-              <span class="navbar-brand mb-0 h1 w-100 text-center" id="navbar-text">Team Profile</span>
-          </nav>
+  <div class="jumbotron jumbotron-fluid bg-warning">
+    <div class="container">
+      <h1 class="display-4 text-center">My Team</h1>
+    </div>
+  </div>
       </header>
       <main>
           <div class="container">
