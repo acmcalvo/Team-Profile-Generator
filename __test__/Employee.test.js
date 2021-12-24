bc9d5728c36885ad334bf8103a424c505c9586fb
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-12-24 08:43:09 -0500

    fix some issues on test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,12 +1,12 @@
 const Employee = require('../lib/Employee');
-// const Password = require("");
+// const Password = require('');
 
-// test("password",() => {
+// test('password',() => {
 // const pass = new Password();
 // expect(pass.length >= 12).toBe(true);
 // });
 
-// // test("password have Number", () => {
+// // test('password have Number', () => {
 // //   const pass = new Password();
 // //   let hasNum = false;
 // //   const nums = '1234567890'.split('');
@@ -20,49 +20,49 @@ const Employee = require('../lib/Employee');
 //   expect(hasNum).toEqual(true);
 // });
 
-test("Create Employee", () => {
+test('Create Employee', () => {
   const emp = new Employee();
-  expect(typeof(emp)).toBe("object");
+  expect(typeof(emp)).toBe('object');
 });
 
-test("Can set name via constructor arguments", () => {
-  const name = "Ale";
+test('Can set name via constructor arguments', () => {
+  const name = 'Ale';
   const emp = new Employee(name);
   expect(emp.name).toBe(name);
 });
 
-test("Can set id via constructor argument", () => {
+test('Can set id via constructor argument', () => {
   const testValue = 100;
-  const emp = new Employee("Esteban", testValue);
+  const emp = new Employee('Esteban', testValue);
   expect(emp.id).toBe(testValue);
 });
 
-test("Can set email via constructor argument", () => {
-  const testValue = "test@test.com";
-  const emp = new Employee("Esteban", 1, testValue);
+test('Can set email via constructor argument', () => {
+  const testValue = 'test@test.com';
+  const emp = new Employee('Esteban', 1, testValue);
   expect(emp.email).toBe(testValue);
 });
 
-test("get name from getName()", () => {
-  const testValue = "Ale";
+test('get name from getName()', () => {
+  const testValue = 'Ale';
   const emp = new Employee(testValue);
   expect(emp.getName()).toBe(testValue);
 });
 
-test("get id from  getId()", () => {
+test('get id from  getId()', () => {
   const testValue = 100;
-  const emp = new Employee("Esteban", testValue);
+  const emp = new Employee('Esteban', testValue);
   expect(emp.getId()).toBe(testValue);
 });
 
-test("get email via getEmail()", () => {
-  const testValue = "test@test.com";
-  const emp = new Employee("Esteban", 2, testValue);
+test('get email via getEmail()', () => {
+  const testValue = 'test@test.com';
+  const emp = new Employee('Esteban', 2, testValue);
   expect(emp.getEmail()).toBe(testValue);
 });
 
-test("getRole() should return \"Employee\"", () => {
-  const testValue = "Employee";
-  const emp = new Employee("Ale", 1, "test@test.com");
+test('getRole() should return \'Employee\'', () => {
+  const testValue = 'Employee';
+  const emp = new Employee('Ale', 1, 'test@test.com');
   expect(emp.getRole()).toBe(testValue);
 });
--- __test__/Engineer.test.js
+++ __test__/Engineer.test.js
@@ -1,1 +1,16 @@
-const Engineer = require('./lib/Engineer')
+const Engineer = require('../lib/Engineer')
+
+test('Engineer Object Created', () => {
+  const engineer = new Engineer ('Alejandra', 48, 'alejandra.argenal@yahoo.com', 'ale0101');
+  expect(engineer.github).toEqual(expect.any(String))
+});
+
+test('Check Github form getGithub', () => {
+  const engineer = new Engineer ('Alejandra', 48, 'alejandra.argenal@yahoo.com', 'ale0101');
+  expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('Get a role from employee', () => {
+  const engineer = new Engineer ('Alejandra', 48, 'alejandra.argenal@yahoo.com', 'ale0101');
+  expect(engineer.getRole()).toEqual('Engineer');
+});
